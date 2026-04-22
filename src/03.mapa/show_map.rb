96c57d53eb2990bf81e7e34b67bcea8f378b5f0a
David Vargas <dvarrui@protonmail.com>
2026-04-22 18:32:14 +0100

    update

--- src/03.mapa/show_map.rb
+++ src/03.mapa/show_map.rb
@@ -3,19 +3,38 @@ def show_map(map)
   puts "Ver mapa:"
   # Todas las filas
   for row in 0..2
-    # todas las columnas
-    for col in 0..2
-      room = map[row][col]
-  
-      if room.dirs.include? "w"
-        door = "-"
-      else
-        door = " "
-      end
-      print room.letter + " " + door + " "
-    end
-  
-    print "\n\n"
+    show_row_room(row, map)
+    show_row_door(row, map)
   end
 end
 
+def show_row_room(row, map)
+  for col in 0..2
+    room = map[row][col]
+  
+    if room.dirs.include? "w"
+      door = "-"
+    else
+      door = " "
+    end
+    print room.letter + " " + door + " "
+  end
+
+  print "\n"
+end
+
+def show_row_door(row, map)
+  for col in 0..2
+    room = map[row][col]
+  
+    if room.dirs.include? "s"
+      door = "|"
+    else
+      door = " "
+    end
+    # print room.letter + " " + door + " "
+    print door + "   "
+  end
+
+  print "\n"
+end
